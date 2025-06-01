0ee10915c3d839ab2505a06b6da2aae83170d64c
Vin <104506147+72knvm@users.noreply.github.com>
2025-06-01 23:26:27 +0700

    Update projectile.py

--- projectile.py
+++ projectile.py
@@ -1,14 +1,24 @@
 import pygame
 
 class Projectile(pygame.sprite.Sprite):
-    def __init__(self, x, y, direction, speed=7):
+    def __init__(self, x, y, direction, speed=4, image=None):
         super().__init__()
-        self.image = pygame.Surface((10, 5))
-        self.image.fill((255, 255, 0))  # warna kuning
-        self.rect = self.image.get_rect(center=(x, y))
-        self.direction = direction  # 1 kanan, -1 kiri
+        if image is not None:
+            self.image = image.copy()
+        else:
+            self.image = pygame.Surface((10, 5))
+            self.image.fill((255, 255, 0))
+
+        self.direction = direction
         self.speed = speed
+
+        if self.direction == -1:
+            self.image = pygame.transform.flip(self.image, True, False)
+
+        self.rect = self.image.get_rect(center=(x, y))
 
     def update(self):
         self.rect.x += self.speed * self.direction
-        # Bisa tambah cek apakah projectile keluar layar, lalu kill()
+        print(f"🚀 Panah bergerak ke {self.rect.x}")
+        if self.rect.right < 0 or self.rect.left > 1600:  # atau batas layar
+            self.kill()
